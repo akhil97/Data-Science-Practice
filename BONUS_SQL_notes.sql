ecc380d5f443b7ce79c41b0333e582526ee09614
akhil97 <akhilm97@gmail.com>
2025-05-13 11:10:16 -0400

    Added day1 notes for SQL based on questions solved on Leetcode

---
+++ BONUS_SQL_notes.sql
@@ -0,0 +1,47 @@
+--In SQL queries, clauses are executed based on the following order:
+    FROM and/or JOIN clause.
+    WHERE clause.
+    GROUP BY clause.
+    HAVING clause.
+    SELECT clause.
+    DISTINCT clause.
+    ORDER BY clause.
+    LIMIT and/or OFFSET clause.
+
+--Using aggregate functions with window functions in SQL
+SUM(quantity) OVER(PARTITION BY id) as total
+
+--Return month number from date
+SELECT EXTRACT(month FROM timestamp '2020-11-30 09:30:20') AS month; returns answer as 11
+
+-- Title Case In Sql
+SELECT INITCAP(item) AS item_title FROM table_name; returns title case for item -> EX:- thai curry becomes Thai Curry
+
+-- COUNT OF NULL values in SQL
+COUNT(*) -> includes NULL values while COUNT(column_name) excludes NULL values
+
+-- Using LIKE command in SQL
+Return data where column name starts with p: WHERE column_name LIKE 'p%'
+
+-- VERY IMPORTANT QUESTION IN INTERVIEWS:- Find second-largest salary of employees
+--First find Maximum Salary of Employees by:-
+SELECT MAX(salary)
+FROM Employees
+--Second find salary that is not Maximum Salary by:-
+SELECT salary
+FROM Employees
+WHERE Salary NOT IN (SELECT MAX(salary) FROM Employees)
+--Lastly, find maximum salary from the answer obtained above. We have salaries that are not maximum salaries. So, maximum of these should give us second maximum by:-
+SELECT MAX(salary)
+FROM Employees
+WHERE Salary NOT IN (SELECT MAX(salary) FROM Employees)
+
+--How to add multiple values of one column in the same line in SQL
+-- There are two ways to solve this:-
+STRING_AGG(column_name, ',') AS combined_value
+-- If for some reason STRING_AGG is not available use this:-
+GROUP_CONCAT(column_name, ',') AS combined_value
+
+-- To find records between a range of dates always use between. For example, you want to find out products ordered in February 2020 then use:-
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+
