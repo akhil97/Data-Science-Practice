770d5c8a69adbc7e1823dd559ef0dd4030b1f784
akhil97 <akhilm97@gmail.com>
2025-06-18 17:10:40 -0400

    Added day37 notes for SQL based on questions solved on DataLemur

--- BONUS_SQL_notes.sql
+++ BONUS_SQL_notes.sql
@@ -398,3 +398,12 @@ SELECT customer_id
 FROM super_cloud
 WHERE product_count = (SELECT COUNT(DISTINCT product_category) FROM products)
 ;
+
+--Find users who shop on more than 3 consecutive days or more
+SELECT DISTINCT t1.user_id
+FROM transactions AS t1 JOIN transactions AS t2
+ON DATE(t2.transaction_date) = DATE(t1.transaction_date) + 1
+JOIN transactions AS t3
+ON DATE(t3.transaction_date) = DATE(t2.transaction_date) + 1
+ORDER BY t1.user_id
+;
