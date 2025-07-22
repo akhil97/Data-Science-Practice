79627ad305d0e201564e1052e0070f574808090f
akhil97 <akhilm97@gmail.com>
2025-07-22 17:54:48 -0400

    Added day72 notes for SQL based on questions solved on stratascratch

--- BONUS_SQL_notes.sql
+++ BONUS_SQL_notes.sql
@@ -968,3 +968,18 @@ select year_month, round((1.0*(total_revenue - prev_month_revenue)/prev_month_re
 from previous_month_revenue
 ;
 -- Use date_format(created_at, '%YYYY-%MM') for SQL and to_char(created_at, 'YYYY-MM') for PostgreSQL.
+
+-- Find the top 5 states with the most 5-star businesses. Output the state name along with the number of 5-star businesses and order records by the number of 5-star businesses in descending order. In case there are ties in the number of businesses, return all the unique states.
+-- If two states have the same result, sort them in alphabetical order.
+with ranked_businesses as (
+select state, count(business_id) as n_businesses,
+dense_rank() over(order by count(business_id) desc) as drnk
+from yelp_business
+where stars = 5
+group by state
+)
+select state, n_businesses
+from ranked_businesses
+where drnk <= 5
+;
+-- If all the states have different results then a limit 5 clause would work but because we want to have the top5 states we use dense rank <= 5 which returns states with the same ranks as well.
