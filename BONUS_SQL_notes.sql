1b9680902bc7daa99d3eb8b5e6440d6f0ac1bc8e
akhil97 <akhilm97@gmail.com>
2025-08-01 22:23:49 -0400

    Added day82 notes for SQL based on questions solved on stratascratch

--- BONUS_SQL_notes.sql
+++ BONUS_SQL_notes.sql
@@ -1117,5 +1117,24 @@ sum_n_messages
 from total_messages
 ;
 
+-- You are given a table named airbnb_host_searches that contains data for rental property searches made by users. Determine the minimum, average, and maximum rental prices for each popularity-rating bucket. A popularity-rating bucket should be assigned to every record based on its number_of_reviews (see rules below).
+-- The host’s popularity rating is defined as below:
+-- •   0 reviews: "New"
+-- •   1 to 5 reviews: "Rising"
+-- •   6 to 15 reviews: "Trending Up"
+-- •   16 to 40 reviews: "Popular"
+-- •   More than 40 reviews: "Hot"
+-- Output host popularity rating and their minimum, average and maximum rental prices. Order the solution by the minimum price.
+select
+case when number_of_reviews = 0 then "New"
+when number_of_reviews between 1 and 5 then "Rising"
+when number_of_reviews between 6 and 15 then "Trending Up"
+when number_of_reviews between 16 and 40 then "Popular"
+when number_of_reviews > 40 then "Hot" end as host_popularity,
+min(price) as min_price, avg(price) as avg_price, max(price) as max_price
+from airbnb_host_searches
+group by host_popularity
+order by min_price;
+
 
 
